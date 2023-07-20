2e417ab036b23d492a8414cba0743b35aaa65640
KaeserOfHonour <kaeser418@gmail.com>
2023-07-20 20:42:43 +0300

    Added testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
@@ -33,4 +36,9 @@ export default defineConfig({
             },
         }),
     ],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: ["./src/setupTest.ts"],
+    },
 });
