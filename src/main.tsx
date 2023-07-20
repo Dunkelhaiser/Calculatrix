75909a37158d7f3dbf868558560a632d5c3b6777
KaeserOfHonour <kaeser418@gmail.com>
2023-07-20 17:10:36 +0300

    Disabled dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 import "./index.css";
 import ThemeContextProvider from "./context/ThemeContext";
+
+if (`${import.meta.env.VITE_NODE_ENV}` === "production") {
+    disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
